e56b427e56041b0d7028b0f09ca5234139c6eb44
Mare94 <Morten.Sandstedt@stud.noroff.no>
2022-11-19 21:17:15 +0100

    created details.js script

---
+++ js/details.js
@@ -0,0 +1,48 @@
+const detailContainer = document.querySelector(".card-result");
+
+const queryString = document.location.search;
+
+const params = new URLSearchParams(queryString);
+const cardId = params.get("cardId");
+
+const url = "https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/classes/druid";
+const corsEnabledUrl = "https://noroffcors.onrender.com/" + url;
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '909fe699e7msh53b75f17f8c54b6p1c3184jsn10ec8f9f33c8',
+		'X-RapidAPI-Host': 'omgvamp-hearthstone-v1.p.rapidapi.com',
+	}
+};
+
+const cardUrl = corsEnabledUrl + cardId;
+
+async function cardDetail(){
+    try{
+
+        const response = await fetch(cardUrl);
+        const details = await response.json();
+
+        console.log(details);
+
+        detailContainer.innerHTML = `
+                                    <h1>${details.name}</h1>
+                                    <h2>
+                                    <div>${details.artist}</div>
+                                    <div>${details.cardSet}</div>
+                                    <div>${details.playerClass}</div>
+                                    <div>${details.type}</div>
+                                    <div>${details.text}</div>
+                                    </h2>
+                                    `
+
+
+
+
+    }
+    catch(error){
+        console.log(error);
+        detailContainer.innerHTML = message("error", error);
+    }
+}
