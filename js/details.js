1a0e6cebb171d61baa49a3833910eec4a1912293
Mare94 <Morten.Sandstedt@stud.noroff.no>
2022-11-20 15:57:52 +0100

    added loading removal

--- js/details.js
+++ js/details.js
@@ -36,15 +36,31 @@ async function getCardDetail(){
 getCardDetail();
 
 function createHtml(details){
-    detailContainer.innerHTML += `
-                                    <div class="detail-result">
-                                    <h1>${details.name}</h1>
-                                    <h2>
-                                    <div>${details.artist}</div>
-                                    <div>${details.cardSet}</div>
-                                    <div>${details.playerClass}</div>
-                                    <div>${details.type}</div>
-                                    <div>${details.text}</div>
-                                    </h2>
-                                    </div>`;
+    
+        for(let i = 0; i < 6; i++){
+
+                if(i === 0){
+                        function removeElementsByClass(className){
+                            const elements = document.getElementsByClassName(className);
+                            while(elements.length > 0){
+                                elements[0].parentNode.removeChild(elements[0])
+                            }
+                        }
+            
+                        removeElementsByClass('loading');
+                    }
+
+                detailContainer.innerHTML += `
+                <div class="detail-result">
+                <h1>${details[i].name}</h1>
+                <h2>
+                <div>Cardset: ${details[i].cardSet}</div>
+                <div>Playerclass: ${details[i].playerClass}</div>
+                <div>Type: ${details[i].type}</div>
+                <div>Card text - ${details[i].text}</div>
+                </h2>
+                </div>`;    
+        }
+    
+        
 }
